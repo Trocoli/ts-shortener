56fd3dadeda10eb2879d18af48b59d4aab71dcca
YgorTrocoli <ygoraraujoca@outlook.com>
2022-09-10 14:05:36 -0300

    frontend added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,26 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
+
+
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (req.nextUrl.pathname.startsWith("/api/get-url/")) {
-    return;
-  }
 
   const slug = req.nextUrl.pathname.split("/").pop();
-  const data = await (
-    await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`)
-  ).json();
+  if(slug === "") {
+    return NextResponse.next()
+  }
 
-  if(data?.url) {
-    return NextResponse.redirect(data.url)
+  const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+  if (slugFetch.status === 404) {
+    console.log(req.nextUrl.origin)
+    return NextResponse.redirect(req.nextUrl.origin);
+  }
+  const data = await slugFetch.json();
+
+  if (data?.url) {
+    return NextResponse.redirect(data.url);
   }
 }
+
+export const config = {
+  matcher: "/:slug",
+};
--- src/pages/api/get-url/[slug].ts
+++ src/pages/api/get-url/[slug].ts
@@ -1,11 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 import { prisma } from "../../../db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const slug = req.query["slug"];
+
   if (!slug || typeof slug !== "string") {
     res.statusCode = 404;
-    res.send(JSON.stringify({ message: "please use with a " }));
+
+    res.send(JSON.stringify({ message: "pls use with a slug" }));
+
     return;
   }
 
@@ -19,8 +23,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   if (!data) {
     res.statusCode = 404;
+
     res.send(JSON.stringify({ message: "slug not found" }));
+
     return;
   }
+
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Cache-Control",
+    "s-maxage=1000000000, stale-while-revalidate"
+  );
+
   return res.json(data);
 };
